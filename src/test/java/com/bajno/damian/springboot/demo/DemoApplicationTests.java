dd868a9e33eda0a7ac1d0d1ca0d044a5083508f6
Damian <bajno.damian96@gmail.com>
2020-11-22 21:52:40 +0100

    Initial commit

---
+++ src/test/java/com/bajno/damian/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajno.damian.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

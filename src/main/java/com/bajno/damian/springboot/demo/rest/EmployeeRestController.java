179981191a67346abc3b6ee65749d3ff951f9d98
Damian <bajno.damian96@gmail.com>
2020-11-23 17:33:51 +0100

    Add create EmployeeRestController

---
+++ src/main/java/com/bajno/damian/springboot/demo/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.bajno.damian.springboot.demo.rest;
+
+import com.bajno.damian.springboot.demo.DAO.EmployeeDAO;
+import com.bajno.damian.springboot.demo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+    // inject employee dao
+
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
